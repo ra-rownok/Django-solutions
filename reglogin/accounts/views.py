7af0aadea47a95937d565774d743da5cf4ab8900
ra_rownok <reshadahamedr001@gmail.com>
2020-03-23 05:02:53 +0600

    no message

--- reglogin/accounts/views.py
+++ reglogin/accounts/views.py
@@ -11,25 +11,23 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
-
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
-                return redirect('register/')
+                return redirect('/register')
             elif User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Taken')
-                return redirect('register/')
+                return redirect('/register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email,
                                                 first_name=first_name,
                                                 last_name=last_name)
                 user.save()
                 print('user created')
-
+                return redirect('/register')
         else:
             messages.info(request, 'password not matching')
-            return redirect('register/')
-        return redirect('/')
+            return redirect('/register')
     else:
         return render(request, 'register.html')
 
