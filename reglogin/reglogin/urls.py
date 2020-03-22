d40f83f2d1f338d618a06dc04947fb9e1be42219
ra_rownok <reshadahamedr001@gmail.com>
2020-03-23 02:05:12 +0600

    Login and Registration

---
+++ reglogin/reglogin/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('register/', views.register, name='register')
+]
