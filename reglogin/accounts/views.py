6089dd496bd521f2191f9ef540f34a03d07878c2
ra_rownok <reshadahamedr001@gmail.com>
2020-03-23 23:30:40 +0600

    no message

--- reglogin/accounts/views.py
+++ reglogin/accounts/views.py
@@ -1,6 +1,23 @@
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 
 def register(request):
@@ -11,23 +28,25 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
+
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username Taken')
-                return redirect('/register')
+                messages.error(request, 'Username Taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Taken')
-                return redirect('/register')
+                messages.error(request, 'Email Taken')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email,
                                                 first_name=first_name,
                                                 last_name=last_name)
                 user.save()
                 print('user created')
-                return redirect('/register')
+                return redirect('login')
         else:
-            messages.info(request, 'password not matching')
-            return redirect('/register')
+            messages.error(request, 'password not matching')
+            return redirect('register')
+
     else:
         return render(request, 'register.html')
 
--- reglogin/reglogin/urls.py
+++ reglogin/reglogin/urls.py
@@ -5,5 +5,6 @@ from accounts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/',views.login, name='login')
 ]
